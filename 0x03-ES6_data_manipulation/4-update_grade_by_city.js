f53554a7f8625ab996b1cf01dc6a4a90ebf8d410
Makarona10 <ahmedneyssi@gmail.com>
2024-07-30 03:55:49 +0300

    Task 4 done

---
+++ 0x03-ES6_data_manipulation/4-update_grade_by_city.js
@@ -0,0 +1,10 @@
+export default function updateStudentGradeByCity(students, city, grades) {
+  return students.filter((s) => (s.location === city))
+    .map((s) => {
+      const grad = grades.filter((g) => g.studentId === s.id);
+      if (grad.length) {
+        return { ...s, grade: grad[0].grade };
+      }
+      return { ...s, grade: 'N/A' };
+    });
+}
