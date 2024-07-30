f4d82a2b9e16d394fb1a21e322c21bedc9eec289
Makarona10 <ahmedneyssi@gmail.com>
2024-07-30 06:45:05 +0300

    Task 8 done

--- 0x03-ES6_data_manipulation/8-clean_set.js
+++ 0x03-ES6_data_manipulation/8-clean_set.js
@@ -1,5 +1,8 @@
 export default function cleanSet(set, startString) {
   let returnString = '';
+  if (!set || !startString || !(set instanceof Set) || typeof startString !== 'string') {
+    return returnString;
+  }
   for (const e of set) {
     if (e.startsWith(startString)) {
       returnString += e.slice(startString.length).concat(' ');
