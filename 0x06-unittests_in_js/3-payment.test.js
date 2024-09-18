a6b8af0c133352f801db82642a0400e2241e32e4
Makarona10 <ahmedneyssi@gmail.com>
2024-09-19 01:51:02 +0300

    Task 3 done

--- 0x06-unittests_in_js/3-payment.test.js
+++ 0x06-unittests_in_js/3-payment.test.js
@@ -1,15 +1,15 @@
-const expect = require('chai').expect;
 const sinon = require('sinon');
-const Utils = require('./utils')
+const Utils = require('./utils');
+const { expect } = require('chai');
 const sendPaymentRequestToApi = require('./3-payment');
 
 describe('sendPaymentRequestToApi', () => {
-  it('', () => {
-    const controller = sinon.spy(Utils);
-  
+  it('sendPaymentRequestToApi uses the calculateNumber method of Utils', () => {
+    const bigBrother = sinon.spy(Utils);
+
     sendPaymentRequestToApi(100, 20);
-    expect(controller.calculateNumber.calledWith('SUM', 100, 20)).to.be.true;
-    expect(controller.calculateNumber.callCount).to.be.equal(1);
-    controller.calculateNumber.restore();
+    expect(bigBrother.calculateNumber.calledWith('SUM', 100, 20)).to.be.true;
+    expect(bigBrother.calculateNumber.callCount).to.be.equal(1);
+    bigBrother.calculateNumber.restore();
   });
 });
