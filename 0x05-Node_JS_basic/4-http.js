001725daa8c907b308b122a199f9cf07dabf0be6
Makarona10 <ahmedneyssi@gmail.com>
2024-09-18 04:08:46 +0300

    Task 4 done

--- 0x05-Node_JS_basic/4-http.js
+++ 0x05-Node_JS_basic/4-http.js
@@ -6,12 +6,15 @@ const app = http.createServer();
 
 app.on('request', (req, res) => {
   const reponse = 'Hello Holberton School!';
+
+  res.setHeader('Content-Type', 'text/plain');
+  res.setHeader('Content-Length', reponse.length);
   res.statusCode = 200;
   res.write(Buffer.from(reponse));
 });
 
 app.listen(PORT, HOST, () => {
-  process.stdout.write(`Server listening on: ${HOST}:${PORT}\n`);
+  process.stdout.write(`Server listening at -> http://${HOST}:${PORT}\n`);
 });
 
 module.exports = app;
