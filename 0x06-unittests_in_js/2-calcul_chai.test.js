ac96c227ecd5263d03c457f5207240288c4897f0
Makarona10 <ahmedneyssi@gmail.com>
2024-09-18 23:20:07 +0300

    Task 2 done

--- 0x06-unittests_in_js/2-calcul_chai.test.js
+++ 0x06-unittests_in_js/2-calcul_chai.test.js
@@ -3,131 +3,131 @@ const calculateNumber = require('./2-calcul');
 describe('calculateNumber', () => {
   describe('type == "SUM"', () => {
     it('', () => {
-      expect(calculateNumber('SUM', 2.0, 2.0)).to.be.equal(4);
+      expect(calculateNumber('SUM', 2.0, 2.0)).to.equal(4);
     });
   
     it('', () => {
-      expect(calculateNumber('SUM', 2.3, 1.8)).to.be.equal(4);
+      expect(calculateNumber('SUM', 2.3, 1.8)).to.equal(4);
     });
   
     it('', () => {
-      expect(calculateNumber('SUM', -2.0, -2.0)).to.be.equal(-4);
+      expect(calculateNumber('SUM', -2.0, -2.0)).to.equal(-4);
     });
   
     it('', () => {
-      expect(calculateNumber('SUM', -2.3, -1.8)).to.be.equal(-4);
+      expect(calculateNumber('SUM', -2.3, -1.8)).to.equal(-4);
     });
   
     it('', () => {
-      expect(calculateNumber('SUM', -2.0, 2.0)).to.be.equal(0);
+      expect(calculateNumber('SUM', -2.0, 2.0)).to.equal(0);
     });
   
     it('', () => {
-      expect(calculateNumber('SUM', 2.0, -2.0)).to.be.equal(0);
+      expect(calculateNumber('SUM', 2.0, -2.0)).to.equal(0);
     });
   
     it('', () => {
-      expect(calculateNumber('SUM', 0.0, 0.0)).to.be.equal(0);
+      expect(calculateNumber('SUM', 0.0, 0.0)).to.equal(0);
     });
   });
   
   describe('type == "SUBTRACT"', () => {
     it('', () => {
-      expect(calculateNumber('SUBTRACT', 2.0, 2.0)).to.be.equal(0);
+      expect(calculateNumber('SUBTRACT', 2.0, 2.0)).to.equal(0);
     });
   
     it('', () => {
-      expect(calculateNumber('SUBTRACT', 2.3, 1.8)).to.be.equal(0);
+      expect(calculateNumber('SUBTRACT', 2.3, 1.8)).to.equal(0);
     });
   
     it('', () => {
-      expect(calculateNumber('SUBTRACT', -2.0, -2.0)).to.be.equal(0);
+      expect(calculateNumber('SUBTRACT', -2.0, -2.0)).to.equal(0);
     });
   
     it('', () => {
-      expect(calculateNumber('SUBTRACT', -2.3, -1.8)).to.be.equal(0);
+      expect(calculateNumber('SUBTRACT', -2.3, -1.8)).to.equal(0);
     });
   
     it('', () => {
-      expect(calculateNumber('SUBTRACT', -2.0, 2.0), -4.0).to.be.equal(-4.0);
+      expect(calculateNumber('SUBTRACT', -2.0, 2.0), -4.0).to.equal(-4.0);
     });
   
     it('', () => {
-      expect(calculateNumber('SUBTRACT', 2.0, -2.0), -4.0).to.be.equal(-4.0);
+      expect(calculateNumber('SUBTRACT', 2.0, -2.0), -4.0).to.equal(-4.0);
     });
   
     it('', () => {
-      expect(calculateNumber('SUBTRACT', 0.0, 0.0), 0).to.be.equal(0);
+      expect(calculateNumber('SUBTRACT', 0.0, 0.0), 0).to.equal(0);
     });
     });
   
   describe('type == "DIVIDE"', () => {
     it('', () => {
-      expect(calculateNumber('DIVIDE', 8.0, 2.0), 4.0).to.be.equal(4.0);
+      expect(calculateNumber('DIVIDE', 8.0, 2.0), 4.0).to.equal(4.0);
     });
   
     it('', () => {
-      expect(calculateNumber('DIVIDE', -7.0, 2.0)).to.be.equal(-3.5);
+      expect(calculateNumber('DIVIDE', -7.0, 2.0)).to.equal(-3.5);
     });
   
     it('', () => {
-      expect(calculateNumber('DIVIDE', 7.0, -2.0)).to.be.equal(-3.5);
+      expect(calculateNumber('DIVIDE', 7.0, -2.0)).to.equal(-3.5);
     });
   
     it('', () => {
-      expect(calculateNumber('DIVIDE', -7.0, -2.0)).to.be.equal(3.5);
+      expect(calculateNumber('DIVIDE', -7.0, -2.0)).to.equal(3.5);
     });
   
     it('', () => {
-      expect(calculateNumber('DIVIDE', 2.0, 2.0)).to.be.equal(1);
+      expect(calculateNumber('DIVIDE', 2.0, 2.0)).to.equal(1);
     });
   
     it('', () => {
-      expect(calculateNumber('DIVIDE', -2.0, -2.0)).to.be.equal(1);
+      expect(calculateNumber('DIVIDE', -2.0, -2.0)).to.equal(1);
     });
   
     it('', () => {
-      expect(calculateNumber('DIVIDE', 2.6, 3.0)).to.be.equal(1);
+      expect(calculateNumber('DIVIDE', 2.6, 3.0)).to.equal(1);
     });
   
     it('', () => {
-      expect(calculateNumber('DIVIDE', 2.4, 2.0)).to.be.equal(1);
+      expect(calculateNumber('DIVIDE', 2.4, 2.0)).to.equal(1);
     });
   
     it('', () => {
-      expect(calculateNumber('DIVIDE', 0.0, 5.0)).to.be.equal(0);
+      expect(calculateNumber('DIVIDE', 0.0, 5.0)).to.equal(0);
     });
   
     it('', () => {
-      expect(calculateNumber('DIVIDE', 0.0, -5.0)).to.be.equal(0);
+      expect(calculateNumber('DIVIDE', 0.0, -5.0)).to.equal(0);
     });
   
     it('', () => {
-      expect(calculateNumber('DIVIDE', 5.0, 0)).to.be.equal('Error');
+      expect(calculateNumber('DIVIDE', 5.0, 0)).to.equal('Error');
     });
   
     it('', () => {
-      expect(calculateNumber('DIVIDE', 5.0, 0.2)).to.be.equal('Error');
+      expect(calculateNumber('DIVIDE', 5.0, 0.2)).to.equal('Error');
     });
   
     it('', () => {
-      expect(calculateNumber('DIVIDE', 5.0, -0.2)).to.be.equal('Error');
+      expect(calculateNumber('DIVIDE', 5.0, -0.2)).to.equal('Error');
     });
   
     it('', () => {
-      expect(calculateNumber('DIVIDE', -5.0, 0)).to.be.equal('Error');
+      expect(calculateNumber('DIVIDE', -5.0, 0)).to.equal('Error');
     });
   
     it('', () => {
-     expect(calculateNumber('DIVIDE', -5.0, 0.2)).to.be.equal('Error');
+     expect(calculateNumber('DIVIDE', -5.0, 0.2)).to.equal('Error');
     });
   
     it('', () => {
-      expect(calculateNumber('DIVIDE', -5.0, -0.2)).to.be.equal('Error');
+      expect(calculateNumber('DIVIDE', -5.0, -0.2)).to.equal('Error');
     });
   
     it('', () => {
-      expect(calculateNumber('DIVIDE', 0.0, 0.0)).to.be.equal('Error');
+      expect(calculateNumber('DIVIDE', 0.0, 0.0)).to.equal('Error');
     });
   });
 });
