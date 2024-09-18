cd7586fd923e25917cad3c3e6dd0beaef4f40ab5
Makarona10 <ahmedneyssi@gmail.com>
2024-09-19 01:46:37 +0300

    Task 3 done

--- 0x06-unittests_in_js/3-payment.test.js
+++ 0x06-unittests_in_js/3-payment.test.js
@@ -1,11 +1,11 @@
 const expect = require('chai').expect;
 const sinon = require('sinon');
-const utils = require('./utils')
+const Utils = require('./utils')
 const sendPaymentRequestToApi = require('./3-payment');
 
 describe('sendPaymentRequestToApi', () => {
   it('', () => {
-    const controller = sinon.spy(utils);
+    const controller = sinon.spy(Utils);
   
     sendPaymentRequestToApi(100, 20);
     expect(controller.calculateNumber.calledWith('SUM', 100, 20)).to.be.true;
