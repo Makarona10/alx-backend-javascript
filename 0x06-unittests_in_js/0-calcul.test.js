4fc6693a507a4e1848a07d9fecf925b83418645f
Makarona10 <ahmedneyssi@gmail.com>
2024-09-18 22:20:35 +0300

    Task 0 done

--- 0x06-unittests_in_js/0-calcul.test.js
+++ 0x06-unittests_in_js/0-calcul.test.js
@@ -2,7 +2,7 @@ const assert = require('assert');
 const calculateNumber = require('./0-calcul');
 describe('calculateNumber', function() {
   it('', function() {
-    assert.strictEqual(calculateNumber(1, 3), 4);
+    assert.equal(calculateNumber(1, 3), 4);
   })
   it('', function() {
     assert.equal(calculateNumber(1.2, 3.7), 5);
@@ -12,5 +12,11 @@ describe('calculateNumber', function() {
   })
   it('', function() {
     assert.equal(calculateNumber(1, 3.7), 5);
+  })
+  it('', function() {
+    assert.equal(calculateNumber(1, 3.7), 5);
+  })
+  it('', function() {
+    assert.equal(calculateNumber(1.65484, 3), 5);
   })
 });
