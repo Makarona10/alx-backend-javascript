0907b44da65e4464106423e4ef49d81af7a838d5
Makarona10 <ahmedneyssi@gmail.com>
2024-09-18 22:55:54 +0300

    Task 1 done

--- 0x06-unittests_in_js/1-calcul.test.js
+++ 0x06-unittests_in_js/1-calcul.test.js
@@ -1,133 +1,133 @@
 const assert = require('assert');
 const calculateNumber = require('./1-calcul');
 describe('calculateNumber', () => {
-    describe('type == "SUM"', () => {
-      it('', () => {
-        assert.equal(calculateNumber('SUM', 2.0, 2.0), 4);
-      });
-  
-      it('', () => {
-        assert.equal(calculateNumber('SUM', 2.3, 1.8), 4);
-      });
-  
-      it('', () => {
-        assert.equal(calculateNumber('SUM', -2.0, -2.0), -4);
-      });
-  
-      it('', () => {
-        assert.equal(calculateNumber('SUM', -2.3, -1.8), -4);
-      });
-  
-      it('', () => {
-        assert.equal(calculateNumber('SUM', -2.0, 2.0), 0);
-      });
-  
-      it('', () => {
-        assert.equal(calculateNumber('SUM', 2.0, -2.0), 0);
-      });
-  
-      it('', () => {
-        assert.equal(calculateNumber('SUM', 0.0, 0.0), 0);
-      });
+  describe('type == "SUM"', () => {
+    it('', () => {
+      assert.equal(calculateNumber('SUM', 2.0, 2.0), 4);
     });
   
-    describe('type == "SUBTRACT"', () => {
-      it('', () => {
-        assert.equal(calculateNumber('SUBTRACT', 2.0, 2.0), 0);
-      });
-  
-      it('', () => {
-        assert.equal(calculateNumber('SUBTRACT', 2.3, 1.8), 0);
-      });
-  
-      it('', () => {
-        assert.equal(calculateNumber('SUBTRACT', -2.0, -2.0), 0);
-      });
-  
-      it('', () => {
-        assert.equal(calculateNumber('SUBTRACT', -2.3, -1.8), 0);
-      });
-  
-      it('', () => {
-        assert.equal(calculateNumber('SUBTRACT', -2.0, 2.0), -4.0);
-      });
-  
-      it('', () => {
-        assert.equal(calculateNumber('SUBTRACT', 2.0, -2.0), 4.0);
-      });
-  
-      it('', () => {
-        assert.equal(calculateNumber('SUBTRACT', 0.0, 0.0), 0);
-      });
+    it('', () => {
+      assert.equal(calculateNumber('SUM', 2.3, 1.8), 4);
     });
   
-    describe('type == "DIVIDE"', () => {
-      it('', () => {
-        assert.equal(calculateNumber('DIVIDE', 8.0, 2.0), 4.0);
-      });
+    it('', () => {
+      assert.equal(calculateNumber('SUM', -2.0, -2.0), -4);
+    });
   
-      it('', () => {
-        assert.equal(calculateNumber('DIVIDE', -7.0, 2.0), -3.5);
-      });
+    it('', () => {
+      assert.equal(calculateNumber('SUM', -2.3, -1.8), -4);
+    });
   
-      it('', () => {
-        assert.equal(calculateNumber('DIVIDE', 7.0, -2.0), -3.5);
-      });
+    it('', () => {
+      assert.equal(calculateNumber('SUM', -2.0, 2.0), 0);
+    });
   
-      it('', () => {
-        assert.equal(calculateNumber('DIVIDE', -7.0, -2.0), 3.5);
-      });
+    it('', () => {
+      assert.equal(calculateNumber('SUM', 2.0, -2.0), 0);
+    });
   
-      it('', () => {
-        assert.equal(calculateNumber('DIVIDE', 2.0, 2.0), 1);
-      });
-  
-      it('', () => {
-        assert.equal(calculateNumber('DIVIDE', -2.0, -2.0), 1);
-      });
-  
-      it('', () => {
-        assert.equal(calculateNumber('DIVIDE', 2.6, 3.0), 1);
-      });
-  
-      it('', () => {
-        assert.equal(calculateNumber('DIVIDE', 2.4, 2.0), 1);
-      });
-  
-      it('', () => {
-        assert.equal(calculateNumber('DIVIDE', 0.0, 5.0), 0);
-      });
-  
-      it('', () => {
-        assert.equal(calculateNumber('DIVIDE', 0.0, -5.0), -0);
-      });
-  
-      it('', () => {
-        assert.equal(calculateNumber('DIVIDE', 5.0, 0), 'Error');
-      });
-  
-      it('', () => {
-        assert.equal(calculateNumber('DIVIDE', 5.0, 0.2), 'Error');
-      });
-  
-      it('', () => {
-        assert.equal(calculateNumber('DIVIDE', 5.0, -0.2), 'Error');
-      });
-  
-      it('', () => {
-        assert.equal(calculateNumber('DIVIDE', -5.0, 0), 'Error');
-      });
-  
-      it('', () => {
-        assert.equal(calculateNumber('DIVIDE', -5.0, 0.2), 'Error');
-      });
-  
-      it('', () => {
-        assert.equal(calculateNumber('DIVIDE', -5.0, -0.2), 'Error');
-      });
-  
-      it('', () => {
-        assert.equal(calculateNumber('DIVIDE', 0.0, 0.0), 'Error');
-      });
+    it('', () => {
+      assert.equal(calculateNumber('SUM', 0.0, 0.0), 0);
     });
   });
+  
+  describe('type == "SUBTRACT"', () => {
+    it('', () => {
+      assert.equal(calculateNumber('SUBTRACT', 2.0, 2.0), 0);
+    });
+  
+    it('', () => {
+      assert.equal(calculateNumber('SUBTRACT', 2.3, 1.8), 0);
+    });
+  
+    it('', () => {
+      assert.equal(calculateNumber('SUBTRACT', -2.0, -2.0), 0);
+    });
+  
+    it('', () => {
+      assert.equal(calculateNumber('SUBTRACT', -2.3, -1.8), 0);
+    });
+  
+    it('', () => {
+      assert.equal(calculateNumber('SUBTRACT', -2.0, 2.0), -4.0);
+    });
+  
+    it('', () => {
+      assert.equal(calculateNumber('SUBTRACT', 2.0, -2.0), 4.0);
+    });
+  
+    it('', () => {
+      assert.equal(calculateNumber('SUBTRACT', 0.0, 0.0), 0);
+    });
+    });
+  
+  describe('type == "DIVIDE"', () => {
+    it('', () => {
+      assert.equal(calculateNumber('DIVIDE', 8.0, 2.0), 4.0);
+    });
+  
+    it('', () => {
+      assert.equal(calculateNumber('DIVIDE', -7.0, 2.0), -3.5);
+    });
+  
+    it('', () => {
+      assert.equal(calculateNumber('DIVIDE', 7.0, -2.0), -3.5);
+    });
+  
+    it('', () => {
+      assert.equal(calculateNumber('DIVIDE', -7.0, -2.0), 3.5);
+    });
+  
+    it('', () => {
+      assert.equal(calculateNumber('DIVIDE', 2.0, 2.0), 1);
+    });
+  
+    it('', () => {
+      assert.equal(calculateNumber('DIVIDE', -2.0, -2.0), 1);
+    });
+  
+    it('', () => {
+      assert.equal(calculateNumber('DIVIDE', 2.6, 3.0), 1);
+    });
+  
+    it('', () => {
+      assert.equal(calculateNumber('DIVIDE', 2.4, 2.0), 1);
+    });
+  
+    it('', () => {
+      assert.equal(calculateNumber('DIVIDE', 0.0, 5.0), 0);
+    });
+  
+    it('', () => {
+      assert.equal(calculateNumber('DIVIDE', 0.0, -5.0), -0);
+    });
+  
+    it('', () => {
+      assert.equal(calculateNumber('DIVIDE', 5.0, 0), 'Error');
+    });
+  
+    it('', () => {
+      assert.equal(calculateNumber('DIVIDE', 5.0, 0.2), 'Error');
+    });
+  
+    it('', () => {
+      assert.equal(calculateNumber('DIVIDE', 5.0, -0.2), 'Error');
+    });
+  
+    it('', () => {
+      assert.equal(calculateNumber('DIVIDE', -5.0, 0), 'Error');
+    });
+  
+    it('', () => {
+     assert.equal(calculateNumber('DIVIDE', -5.0, 0.2), 'Error');
+    });
+  
+    it('', () => {
+      assert.equal(calculateNumber('DIVIDE', -5.0, -0.2), 'Error');
+    });
+  
+    it('', () => {
+      assert.equal(calculateNumber('DIVIDE', 0.0, 0.0), 'Error');
+    });
+  });
+});
